0a5555f61731a42c11c5aade778a48f303dfecb1
Caleb Avery <arbixal@hotmail.com>
2021-10-17 21:21:11 +1100

    Added some styling tweaks and a report bug button.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ import { ReportLoad } from './ReportLoad';
 class App extends Component {
   render() {
     return (
+      <>
+      <div className="nav-bar">
+        <div className="bug-report">
+          <a href="https://github.com/arbixal/wow-classic-heal-analyser/issues/new" target="_blank" rel="noreferrer"><img alt="Github Logo" src="GitHub-Mark-Light-32px.png"></img> Report a Bug</a>
+        </div>
+        <h1>WoW Classic Log Analyser</h1>
+      </div>
       <Router>
           <Switch>
             <Route exact path="/">
@@ -29,6 +36,7 @@ class App extends Component {
             </Route>
           </Switch>
       </Router>
+      </>
     );
   }
 }
